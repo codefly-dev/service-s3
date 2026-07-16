ba33ab0c0e7fd2ac12fa6afe2f80ea67460e13fc
Antoine Toussaint <toussaint.antoine@gmail.com>
2026-07-16 15:24:23 -0400

    feat: add container audit and SBOM support

--- builder.go
+++ builder.go
@@ -11,7 +11,6 @@ import (
 	"github.com/codefly-dev/core/wool"
 
 	"github.com/codefly-dev/core/agents/services"
-	"github.com/codefly-dev/core/agents/services/audit"
 	"github.com/codefly-dev/core/agents/services/upgrade"
 	builderv0 "github.com/codefly-dev/core/generated/go/codefly/services/builder/v0"
 )
@@ -76,11 +75,13 @@ func (s *Builder) Build(ctx context.Context, req *builderv0.BuildRequest) (*buil
 func (s *Builder) Audit(ctx context.Context, req *builderv0.AuditRequest) (*builderv0.AuditResponse, error) {
 	defer s.Wool.Catch()
 	ctx = s.Wool.Inject(ctx)
-	res, err := audit.Docker(ctx, image.FullName())
-	if err != nil {
-		return s.Builder.AuditError(err)
-	}
-	return s.Builder.AuditResponse(res.Findings, res.Outdated, res.Tool, res.Language)
+	return s.Builder.AuditContainer(ctx, req, image.FullName())
+}
+
+func (s *Builder) SBOM(ctx context.Context, _ *builderv0.SBOMRequest) (*builderv0.SBOMResponse, error) {
+	defer s.Wool.Catch()
+	ctx = s.Wool.Inject(ctx)
+	return s.Builder.SBOMContainer(ctx, image.FullName())
 }
 
 // Upgrade reports a newer s3 tag (within current major unless --major).
